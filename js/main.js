aa0b49551e1d521265d719a9648a084f675620c4
boryanakrasteva <115458613+boryanakrasteva@users.noreply.github.com>
2022-10-11 17:04:36 +0300

    updated

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('#city').select2({
+        placeholder: "Изберете град",
+    });
+});
+
+$(document).ready(function() {
+    $('#restaurant').select2({
+        placeholder: "Изберете ресторант",
+    });
+});
+
